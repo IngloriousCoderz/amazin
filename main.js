8900edf19cdd1502ec805b67c1a4ed45b4a23a56
IceOnFire <antony.mistretta@gmail.com>
2016-01-23 23:26:48 +0100

    Add samples and open Electron window

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
